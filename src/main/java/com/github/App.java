380ad0f23c6a013d3db9d40f23d58a512549edee
vivekgupta3011 <vivekgupta3011@yahoo.com>
2021-09-19 19:28:15 +0530

    Update App.java

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello Edited!" );
+        System.out.println( "Hello Test!" );
     }
 }
 
